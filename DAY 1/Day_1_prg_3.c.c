91628a2d0432106b2a102db62bdac4c1f742a57d
Ajithkumar2604 <108972592+Ajithkumar2604@users.noreply.github.com>
2022-07-11 20:51:26 +0530

    Add files via upload

---
+++ DAY 1/Day_1_prg_3.c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float num;
+printf("Enter the radius of the gardan ");
+scanf("%f",&num);
+//to finding the radiusof garden to use data type as float.
+printf("Area is %f",0.314*num*num);
+return 0;
+}
